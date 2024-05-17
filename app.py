3d2f2986e2e6280662bfec51342123eb520b839c
jayakrishna1204 <jayakrishnap1509@gmail.com>
2024-05-17 09:23:26 +0530

    App

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ from flask_mysqldb import MySQL
 app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = '*****'
-app.config['MYSQL_PASSWORD'] = '*****'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Krishna@1509#'
 app.config['MYSQL_DB'] = 'to_do_list'
 
 mysql = MySQL(app)
